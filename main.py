d54a111d829d3acc2ad91a18b8bd6c5b0aeb1bc6
Seth <jsbumagat37@gmail.com>
2021-01-17 10:39:16 -0700

    WalkIn in ChrisTmAS wOnDerLAnd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 countries = [
 	'Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia',
@@ -26,6 +27,17 @@ countries = [
 	'West Bank and Gaza', 'Yemen', 'Zambia', 'Zimbabwe', 'US', 'Global'
 ]
 
+# To create list of countries unavailable to be interpreted by bot
+special_case = []
+n = 0
+for country in countries:
+	splitter = countries[n].split()
+	if len(splitter) == 1:
+		special_case.append(countries[n])
+	else:
+		None
+	n = n + 1
+
 
 #function to get country data
 def get_data(country = 'All'):
@@ -46,7 +58,7 @@ def num_prov(country):
 		print("\nTalking2U: Would you like to see the statistics for a specific location or demographic? Please say yes or no\n")
 		ask = str(input(name + ": "))
 		if ask.upper() == "YES":
-			print("\nTalking2U: Please choose one of the locations or demographics below! \n")
+			print("\nTalking2U: Please choose one of the locations or demographics below! Please spell it correctly! I'm case-sensitive hehe\n")
 			var = list(COVID_Call)[1:]
 			for item in var:
 				print(item)
@@ -70,13 +82,13 @@ def stats(data, country, key, province): #this takes the whole country data, tak
 		else:
 			print("\n")
 			print(f'''Talking2U: In {province} there are currently {data[province]["confirmed"]} cases, {data[province]["recovered"]} cases have recovered, 
-	   and there have been {data[province]["deaths"]} mortalities.''')
+	   and there have been {data[province]["deaths"]} mortalities.\n''')
 	elif key == "RECOVERED":
 		print(f'''\nTalking2U: Of the {data[province]["confirmed"]} confirmed cases, there are currently {data[province]["recovered"]} recovered cases!''')
 	elif key == 'CONFIRMED':
 		print(f'''\nTalking2U: As of the recent statistics, there are currently {data[province]["confirmed"]} confirmed COVID-19 cases.''')
 	else:
-		print(f'''Talking2U: As of the recent statistics, have been {data[province]["deaths"]} mortalities.''')
+		print(f'''\nTalking2U: As of the recent statistics, have been {data[province]["deaths"]} mortalities.''')
 	print(f'''\nTalking2U: Remember to wear a mask, socially distance, and follow your local COVID-19 
 	   guidelines and legislations! Stay safe and healthy {name}!''')
 
@@ -85,12 +97,16 @@ def stats(data, country, key, province): #this takes the whole country data, tak
 
 def symptom_check(): #If you can, pls check this to check the formatting <3
 	print("\nTalking2U: Alright, let me fetch my assessment tool!")
+	time.sleep(0.5)
 	print("\nTalking2U: ...")
+	time.sleep(1.5)
 	print("""\nTalking2U: Here it is! The following questionnaire will help you determine if you require 
 further assessment for COVID-19 per the Government of Canada Self-Assessment Tool""")
+	time.sleep(1)
 	print("""\nTalking2U: Please note this checker is not a replacement for a COVID test!
-	If you are experiencing a life-threatening emergency, call 911 NOW. 
-	Otherwise, please answer the following questions.""")
+	   If you are experiencing a life-threatening emergency, call 911 NOW. 
+	   Otherwise, please answer the following questions.""")
+	time.sleep(3)
 	print("""\nTalking2U: Are you experiencing any of the following symptoms:
 
 	   - severe difficulty breathing
@@ -102,6 +118,7 @@ further assessment for COVID-19 per the Government of Canada Self-Assessment Too
 	   Please answer yes or no\n""")
 	symptoms = str(input(name + ': ')).upper()
 	symp_list = symptoms.split(" ")
+	time.sleep(0.5)
 	if 'YES' in symp_list:
 		print("""\nTalking2U: You need urgent medical help - please call 911 or go to
            your nearest emergency department. These symptoms require immediate attention, 
@@ -123,10 +140,13 @@ further assessment for COVID-19 per the Government of Canada Self-Assessment Too
 	   Please answer yes or no\n""")
 		symptoms = str(input(name + ': ')).upper()
 		symp_list = symptoms.split()
+		time.sleep(0.5)
 		if 'YES' in symp_list:
 			print("""\nTalking2U: Please self-isolate. The Public Health Agency of Canada asks
 	   anyone with the above symptoms to please self-isolate as a precaution""")
+			time.sleep(0.5)
 			print("\nTalking2U: ...")
+			time.sleep(2.5)
 			print("""\nTalking2U: To determine your next steps, please utilize the COVID-19 Self-Assessment tool on
 	   the Government of Canada website.""")
 		else:
@@ -140,7 +160,13 @@ def main():
 	#check if valid country
 	valid_country = False
 	country_input = ['']
-	print('\nTalking2U: What country would you like check out?\n')
+	print('\nTalking2U: What country would you like check out? Please choose from the list below!')
+	print('\nTalking2U: If you want to check out the global statistics instead, ask for global!\n')
+	spc = list(special_case)[1:]
+	time.sleep(1)
+	for item in spc:
+		print(item)
+	print(' ')
 	while not valid_country:
 		country_input = input(name + ': ').split()
 		country_input = [x.title() for x in country_input]
@@ -150,7 +176,7 @@ def main():
 				country_input = ['US']
 		valid_country = any(item in country_input for item in countries)
 		if not valid_country:
-			print('\nTalking2U: Please enter a valid country.\n')
+			print("\nTalking2U: Sorry, I didn't quite understand that. Did you choose a country from the list?\n")
 
 	for item in countries:
 		if item in country_input:
