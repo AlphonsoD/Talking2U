b432340fa3d2565603307a30e9c3a08cea4e2945
acharyarucha <acharyarucha01@gmail.com>
2021-01-16 17:23:23 -0700

    clean slates

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-print('hello!')
-print("HELLOOOOOO!")
-print('Wassup yo')
+import requests
+import json
+
+#function to get country data
+
+
+
+
+#function for number of provinces
+
+
+
+
+
+
+# what stat we want function
+
+
+
+
+#main function
+
+
+
+# if __name__=='__main__'
+#main()
+
+
+
