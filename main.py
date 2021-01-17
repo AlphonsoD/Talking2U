79e7089943ee51643ed19f4344e361d169ee983a
Alphonso Dineros <alphonso@ualberta.ca>
2021-01-16 17:50:14 -0700

    the lion sleeps tonight

--- main.py
+++ main.py
@@ -19,11 +19,17 @@ import json
 
 
 #main function
+def main():
+	print('Hello! I am Talking2U, a chat bot desgined to give statstics about COVID-19 from around the world.')
+	country = input('What country would you like check out? Please enter a country: ').strip().title()
+	data = get_data(country)
+	province = num_prov(country)
+	print('What would you like to see specifically? Choose either "recovered", "confirmed", or "deaths".')
+	user_said = input('Enter your selection: ')
+	stats(data, user_said, province = 'All')
 
-
-
-# if __name__=='__main__'
-#main()
+if __name__=='__main__':
+	main()
 
 
 
